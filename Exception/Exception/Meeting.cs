e193d766af04b3edc9d23ad5456635cd3d2da15c
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-04-04 01:05:18 +0400

    incomplateException

---
+++ Exception/Exception/Meeting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    class Meeting
+    {
+        public int FromDate { get; set; }
+        public int ToDate { get; set; }
+        public string FullName { get; set; }
+
+        public Meeting(int fromDate,int toDate,string fullName)
+            
+        {
+            FromDate = fromDate;
+            ToDate = toDate;
+            FullName = fullName;
+        }
+    }
+}
---
+++ Exception/Exception/MeetingSchedule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    class MeetingSchedule
+    {
+        public Meeting[] meetings = new Meeting[16];
+        public int MeetingDay = 0;
+        public void SetMeeting(string fullName,DateTime from,DateTime to)
+        {
+            try
+            {
+                if (from>=to)
+                Console.WriteLine(WrongDateIntervalException ());   
+                for (int i=0; i<MeetingDay;i++)
+            }
+            catch 
+            {
+                Console.WriteLine(ReservedDateInterval ());
+            }
+            
+        }
+    }
+}
